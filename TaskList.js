f0c2bc3b472e50e380e55649477ba82d41e49274
Bram Borggreve <borggreve@gmail.com>
2016-04-24 15:05:47 -0500

    Add a from for adding todo's

--- TaskList.js
+++ TaskList.js
@@ -3,14 +3,34 @@ import React from 'react-native'
 const {
   ListView,
   Text,
+  TouchableHighlight,
   View,
 } = React
 
 const styles = React.StyleSheet.create({
   container: {
     paddingTop: 40,
+    backgroundColor: '#F7F7F7',
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  button: {
+    height: 60,
+    borderColor: '#05A5D1',
+    borderWidth: 2,
+    backgroundColor: '#333',
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FAFAFA',
+    fontSize: 20,
+    fontWeight: '600'
   }
 })
+
+import TaskRow from './TaskRow'
 
 class TaskList extends React.Component {
   constructor (props, context) {
@@ -25,9 +45,18 @@ class TaskList extends React.Component {
     }
   }
 
-  renderRow(todo) {
+  componentWillReceiveProps (nextProps) {
+    const dataSource = this
+      .state
+      .dataSource
+      .cloneWithRows(nextProps.todos)
+
+    this.setState({ dataSource })
+  }
+
+  renderRow (todo) {
     return (
-      <Text>{todo.task}</Text>
+      <TaskRow todo={todo} />
     )
   }
 
@@ -35,14 +64,27 @@ class TaskList extends React.Component {
     return (
       <View style={styles.container}>
         <ListView
-          dataSource={this.state.dataSource}
-          renderRow={this.renderRow.bind(this)}/>
+            dataSource={this.state.dataSource}
+            key={this.props.todos}
+            renderRow={this.renderRow.bind(this)}
+        />
+        <TouchableHighlight
+            onPress={this.props.onAddStarted}
+            style={styles.button}
+        >
+          <Text
+              style={styles.buttonText}
+          >
+            Add one
+          </Text>
+        </TouchableHighlight>
       </View>
     )
   }
 }
 
 TaskList.propTypes = {
+  onAddStarted: React.PropTypes.func.isRequired,
   todos: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
 }
 
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import React from 'react-native'
 import TaskList from './TaskList'
+import TaskForm from './TaskForm'
 
 let {
   AppRegistry,
   Component,
+  Navigator,
 } = React
 
 class PluralTodo extends Component {
@@ -18,8 +20,58 @@ class PluralTodo extends Component {
     }
   }
 
+  onAddStarted () {
+    this.nav.push({
+      name: 'taskform'
+    })
+  }
+
+  onCancel () {
+    this.nav.pop()
+  }
+
+  onAdd (task) {
+    console.log('A task was added', task)
+    this.state.todos.push({ task })
+    console.log(this.state.todos)
+    this.setState({ todos: this.state.todos })
+    this.nav.pop()
+  }
+
+  renderScene (route, nav) {
+    switch (route.name) {
+    case 'taskform':
+      return (
+          <TaskForm
+              onAdd={this.onAdd.bind(this)}
+              onCancel={this.onCancel.bind(this)}
+          />
+      )
+    default:
+      return (
+        <TaskList
+            onAddStarted={this.onAddStarted.bind(this)}
+            todos={this.state.todos}
+        />
+      )
+    }
+  }
+
+  configureScene () {
+    return Navigator.SceneConfigs.FloatFromBottom
+  }
+
   render () {
-    return <TaskList todos={this.state.todos} />
+    return (
+      <Navigator
+          configureScene={this.configureScene}
+          initialRoute={{ name: 'tasklist', index: 0 }}
+          ref={((nav) => {
+            this.nav = nav
+          })}
+          renderScene={this.renderScene.bind(this)}
+      />
+    )
   }
 
 }
